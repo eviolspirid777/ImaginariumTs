963734df73d44d6b80f544c5d242512c0ccb6ab1
eviolspirid777 <eviolspirid@gmail.com>
2024-01-04 11:53:59 +0300

    комментарий смешной

--- Imaginarium.service/Imaginarium/Imaginarium/Controllers/Card.cs
+++ Imaginarium.service/Imaginarium/Imaginarium/Controllers/Card.cs
@@ -10,7 +10,7 @@ namespace Imaginarium.Controllers
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int id { get; set; }
 		public string CardUrl { get; set; }
-		public bool InGame { get; set; }
+		public bool InGame { get; set; } //erjgejtrg
 		public string CardName { get; set; }
 		public Room? Room { get; set; }
 	}
