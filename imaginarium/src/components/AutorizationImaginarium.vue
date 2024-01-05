debd499702946e9b3c9518da9b56fde24548813f
BiQndy <atashol228@mail.ru>
2024-01-06 02:50:02 +0300

    импрувнул интерфейс окна авториации

--- imaginarium/src/components/AutorizationImaginarium.vue
+++ imaginarium/src/components/AutorizationImaginarium.vue
@@ -3,16 +3,19 @@
     <div class="modal-wrapper">
       <div class="modal-container">
         <span class="modal-container-header">Введите имя</span>
-        <span class="modal-container-exit" @click="hideModalWindow">x</span>
+    
+        <span class="modal-container-exit" @click="hideModalWindow"><i class="fa-solid fa-circle-xmark"></i></span>
       </div>
       <span class="modal-wrapper-name">
-        Имя <input v-model="playerName"/>
+        <input v-model="playerName"/>
       </span>
       <span class="modal-wrapper-error" v-if="error.length > 0">
         {{ error }}
       </span>
       <div class="modal-wrapper-button">
-        <button type="button" @click="login">Enter</button>
+        <button type="button" @click="login" @mouseover="hoverIcon = true" @mouseleave="hoverIcon = false">
+          <i :class="{'fa-solid fa-check fa-xl': !hoverIcon, 'fa-solid fa-check fa-xl fa-beat-fade': hoverIcon}" style="color: green;"></i>
+        </button>
       </div>
     </div>
   </div>
@@ -23,6 +26,7 @@ import axios from "axios"
 
 const emits = defineEmits(["hideModal", "closeWindow"]);
 
+const hoverIcon = ref(false);
 const error = ref("")
 const playerName = ref("")
 
@@ -64,10 +68,10 @@ const login = async () => {
   &-mask{
     position: absolute;
     display: flex;
-    top: 140;
+    top: 0px;
     left: 0;
     width: 100%;
-    height: 89%;
+    height: 100%;
     background-color: rgba(0, 0, 0, 0.6);
     align-items: center;
     justify-content: center;
@@ -75,16 +79,28 @@ const login = async () => {
 
   &-wrapper{
     width: 400px;
-    height: 145px;
+    height: 150px;
+    position: relative; 
     background-color: #000000;
     display: flex;
     justify-content: flex-start;
     align-items: center;
     flex-flow: column nowrap;
     border-radius: 10px;
-    box-shadow: 1px 1px 10px #b8c952;
+    box-shadow: 0px 0px 30px #dd1313;
     color: wheat;
     font-family: Arial, Helvetica, sans-serif;
+
+    animation: pulseShadow 1.5s infinite alternate ease-in-out;
+    @keyframes pulseShadow {
+      from {
+        box-shadow: 0px 0px 15px #dd1313;
+      }
+      to {
+        box-shadow: 0px 0px 60px #dd1313;
+      }
+    }
+
     &-name{
       font-size: 20px;
       cursor: default;
@@ -100,51 +116,54 @@ const login = async () => {
     &-button{
       display: flex;
         button{
-        margin-right: 80px;
-        margin-left: 80px;
         margin-top: 10px;
         min-height: 30px;
         width: 80px;
         background-color: wheat;
         font-weight: 800;
         border: 0 solid;
-        border-radius: 1px;
+        border-radius: 6px;
         &:hover{
           background-color: rgb(218, 195, 151);
-          box-shadow: 1px 1px 10px #b8c952;
+          box-shadow: 0px 0px 12px #0ffc22;
         }
       }
     }
   }
 
-  &-container{
-    display: flex;
-    width: 360px;
-    justify-content: space-between;
-    border-radius: 10px;
-    background-color: rgba(0, 0, 0, 1);
-    padding: 20px;
-    padding-bottom: 20px;
-    text-align: center;
-    color: wheat;
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 22px;
-    &-header{
-      padding-left: 30%;
-      cursor: default;
-      user-select: none;
-    }
-    &-exit{
-      width: 20px;
-      background-color: wheat;
-      color: black;
-      user-select: none;
-      &:hover{
-        background-color: rgb(218, 195, 151);
-        cursor: pointer;
-        box-shadow: 1px 1px 10px #b8c952;
-      }
+  &-container {
+  position: relative;
+  display: flex;
+  width: 360px;
+  justify-content: space-between;
+  border-radius: 10px;
+  background-color: rgba(0, 0, 0, 1);
+  padding: 20px;
+  padding-bottom: 20px;
+  text-align: center;
+  color: wheat;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 22px;
+
+  &-header {
+    margin: 0 auto; // Центрируем текст
+    cursor: default;
+    user-select: none;
+  }
+
+  &-exit {
+    color: red;
+    margin: 10px;
+    right: 0;
+    top: 0;
+    user-select: none;
+    position: absolute;
+
+    &:hover {
+      cursor: pointer;
     }
   }
+}
+
 }
 </style>
